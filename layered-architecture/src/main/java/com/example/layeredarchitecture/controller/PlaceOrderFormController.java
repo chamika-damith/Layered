aab4ded9b51785becb00d273d9eeab782c6c7274
chamika-damith <chamikadamith9@gmail.com>
2023-12-16 16:52:53 +0530

    generate new order id

--- layered-architecture/src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/controller/PlaceOrderFormController.java
@@ -383,12 +383,7 @@ public class PlaceOrderFormController {
 
     public ItemDTO findItem(String code) {
         try {
-            Connection connection = DBConnection.getDbConnection().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
-            pstm.setString(1, code);
-            ResultSet rst = pstm.executeQuery();
-            rst.next();
-            return new ItemDTO(code, rst.getString("description"), rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
+            return orderDAO.findItem(code);
         } catch (SQLException e) {
             throw new RuntimeException("Failed to find the Item " + code, e);
         } catch (ClassNotFoundException e) {
--- layered-architecture/src/main/java/com/example/layeredarchitecture/dao/impl/OrderDAOImpl.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/dao/impl/OrderDAOImpl.java
@@ -1,11 +1,9 @@
 package com.example.layeredarchitecture.dao.impl;
 
 import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.ItemDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class OrderDAOImpl {
     public String generateNextNewOrderId() throws SQLException, ClassNotFoundException {
@@ -14,5 +12,14 @@ public class OrderDAOImpl {
         ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
 
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
+    }
+
+    public ItemDTO findItem(String code) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
+        pstm.setString(1, code);
+        ResultSet rst = pstm.executeQuery();
+        rst.next();
+        return new ItemDTO(code, rst.getString("description"), rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
     }
 }
