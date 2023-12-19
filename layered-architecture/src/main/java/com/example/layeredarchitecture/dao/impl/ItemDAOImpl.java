733d950c5c6fb024478e4be0784f4a394f591d20
chamika-damith <chamikadamith9@gmail.com>
2023-12-19 12:26:50 +0530

    Sqlutil save Item

--- layered-architecture/src/main/java/com/example/layeredarchitecture/dao/impl/ItemDAOImpl.java
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/dao/impl/ItemDAOImpl.java
@@ -1,6 +1,7 @@
 package com.example.layeredarchitecture.dao.impl;
 
 import com.example.layeredarchitecture.dao.ItemDAO;
+import com.example.layeredarchitecture.dao.SQLutil;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.ItemDTO;
 import com.example.layeredarchitecture.view.tdm.ItemTM;
@@ -12,9 +13,7 @@ import java.util.ArrayList;
 public class ItemDAOImpl implements ItemDAO {
     @Override
     public ArrayList<ItemDTO> allItems() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT * FROM Item");
+        ResultSet rst = SQLutil.execute("SELECT * FROM Item");
         ArrayList<ItemDTO> itemDTOS=new ArrayList<>();
         while (rst.next()) {
             ItemDTO itemDTO=new ItemDTO(rst.getString("code"), rst.getString("description"), rst.getBigDecimal("unitPrice"), rst.getInt("qtyOnHand"));
@@ -24,13 +23,7 @@ public class ItemDAOImpl implements ItemDAO {
     }
     @Override
     public boolean saveItem(String code, String description, int qtyOnHand, BigDecimal unitPrice) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
-        pstm.setString(1, code);
-        pstm.setString(2, description);
-        pstm.setBigDecimal(3, unitPrice);
-        pstm.setInt(4, qtyOnHand);
-        return pstm.executeUpdate() > 0;
+        return SQLutil.execute("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)",code,description,qtyOnHand,unitPrice);
     }
     @Override
     public boolean updateItem(String code, String description, int qtyOnHand, BigDecimal unitPrice) throws SQLException, ClassNotFoundException {
